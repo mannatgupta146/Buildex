f548517f0d554c7496c60bf598f0274122edd43d
Mannat Gupta <mannatgupta146@gmail.com>
2026-05-16 01:33:41 +0530

    feat(router): add router deployment and service configurations, update ingress rules

--- sandbox/router/src/app.js
+++ sandbox/router/src/app.js
@@ -6,6 +6,20 @@ const app = express();
 
 app.use(morgan('combined'));
 
+app.get('/api/status/healthz', (req, res) => {
+    res.status(200).json({
+        message: 'Sandbox Router is healthy',
+        status: 'ok'
+    });
+});
+
+app.get('/api/status/readyz', (req, res) => {
+    res.status(200).json({
+        message: 'Sandbox Router is ready',
+        status: 'ok'
+    });
+});
+
 app.use((req, res, next) => {
     const host = req.headers.host;
     const sandboxId = host.split('.')[0];
