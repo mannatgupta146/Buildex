edcf4891e6345bf298e5e944dc6e4bcb111396ca
Mannat Gupta <mannatgupta146@gmail.com>
2026-05-16 19:18:45 +0530

    feat(app): add file listing endpoint and import fs module; create Dockerfile and .dockerignore

--- sandbox/agent/src/app.js
+++ sandbox/agent/src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import morgan from 'morgan';
+import fs from 'fs';
 
 const app = express();
 
@@ -7,11 +8,23 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const WORKSPACE_DIR = '/workspace';
+
 app.get('/api/agent/health', (req, res) => {
     res.status(200).json({
         message: 'Sandbox agent API is healthy',
         status: 'ok'
     });
 });
+
+app.get('/list-files', async (req, res) => {
+    const elements = await fs.promises.readdir(WORKSPACE_DIR);
+    res.status(200).json({ 
+        message: 'Elements in working directory',
+        elements
+    });
+
+
+})
 
 export default app;
