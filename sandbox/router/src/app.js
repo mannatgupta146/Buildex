03712d436cbe7ce9ad91ec96809b46ce408ffdf7
Mannat Gupta <mannatgupta146@gmail.com>
2026-05-16 15:31:36 +0530

    fix(router): clean up formatting and improve health check responses in router service

--- sandbox/router/src/app.js
+++ sandbox/router/src/app.js
@@ -1,37 +1,39 @@
 import express from 'express';
 import morgan from 'morgan';
-import { createProxyMiddleware } from 'http-proxy-middleware';
+import { createProxyMiddleware } from "http-proxy-middleware"
 
 const app = express();
-
 app.use(morgan('combined'));
 
 app.get('/api/status/healthz', (req, res) => {
-    res.status(200).json({
-        message: 'Sandbox Router is healthy',
-        status: 'ok'
-    });
-});
+    res.status(200).json({ status: 'ok' });
+})
 
 app.get('/api/status/readyz', (req, res) => {
-    res.status(200).json({
-        message: 'Sandbox Router is ready',
-        status: 'ok'
-    });
-});
+    res.status(200).json({ status: 'ready' });
+})
+
+const proxies = {}
+
+function getProxy(sandboxId) {
+
+    const target = `http://sandbox-service-${sandboxId}`; // Construct target URL based on sandboxId
+
+    if (!proxies[ sandboxId ]) {
+        proxies[ sandboxId ] = createProxyMiddleware({
+            target,
+            changeOrigin: true,
+            ws: true,
+        })
+    }
+    return proxies[ sandboxId ];
+}
 
 app.use((req, res, next) => {
     const host = req.headers.host;
-    const sandboxId = host.split('.')[0];
+    const sandboxId = host.split('.')[ 0 ];
 
-    const target = `http://sandbox-service-${sandboxId}`;
-
-    return createProxyMiddleware({
-        target,
-        changeOrigin: true,
-        ws: true,
-
-    })(req, res, next);
+    return getProxy(sandboxId)(req, res, next);
 })
 
 export default app
