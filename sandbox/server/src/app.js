0c949ec7e58e2e59bdc563c79d2feee9411ab244
Mannat Gupta <mannatgupta146@gmail.com>
2026-06-03 17:04:37 +0530

    redis configuration and environment variable setup for sandbox deployment

--- sandbox/server/src/app.js
+++ sandbox/server/src/app.js
@@ -4,6 +4,7 @@ import { createPod } from "./kubernetes/pod.js"
 import { createService } from "./kubernetes/service.js"
 import { v7 as uuid } from "uuid"
 import http from "http"
+import {createSandboxKey} from "./config/redis.js"
 
 const app = express()
 
@@ -21,7 +22,11 @@ app.get("/api/sandbox/health", (req, res) => {
 app.post("/api/sandbox/start", async (req, res) => {
   const sandboxId = uuid()
 
-  await Promise.all([createPod(sandboxId), createService(sandboxId)])
+  await Promise.all([
+    createPod(sandboxId), 
+    createService(sandboxId),
+    createSandboxKey(sandboxId)
+  ])
 
   return res.status(201).json({
     message: "Sandbox environment created successfully",
--- sandbox/server/src/config/redis.js
+++ sandbox/server/src/config/redis.js
@@ -1,1 +1,21 @@
-import redis from 'redis';
+import Redis from 'ioredis';
+
+const redis = new Redis(process.env.REDIS_URL);
+
+const subscriber = new Redis(process.env.REDIS_URL);
+
+export async function createSandboxKey(sandboxId) {
+    await redis.set(`sandbox:${sandboxId}`, JSON.stringify({
+        status: "active",
+    }), "EX", 60 * 2); // Set key to expire in 2 minutes
+}
+
+subscriber.config("SET", "notify-keyspace-events", "Ex");
+
+subscriber.subscribe("__keyevent@0__:expired")
+
+subscriber.on("message", (channel, key) => {
+    console.log(`Key expired: ${key}`);
+})
+
+export default {subscriber};
